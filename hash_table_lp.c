411aeb51715e71af09af7a70a18b1c26faaa11ac
gliontos <sdi1600094@di.uoa.gr>
2019-02-14 16:09:43 +0200

    Added utils.c

--- hash_table_lp.c
+++ hash_table_lp.c
@@ -17,8 +17,10 @@ static inline HT_LP_Record_Entry HT_LP_Record_Entry_Create(uint16_t key, size_t 
     };
 }
 
-int HT_LP_Create(HT_LP *ht_out, const size_t value_size, const size_t buckets, Hash_Function hash_function) {
+int HT_LP_Create(HT_LP *ht_out, const size_t buckets, Hash_Function hash_function) {
     if (!hash_function) return ENOHASHFUNCTION;
+    void *records = __MALLOC(buckets * buckets, void*);
+    if (!records) return ENOMEM;
     HT_LP_Record_Entry **table = __MALLOC(buckets, HT_LP_Record_Entry*);
     if (!table) return ENOMEM;
     for (size_t i = 0U; i != buckets; ++i) {
@@ -31,29 +33,30 @@ int HT_LP_Create(HT_LP *ht_out, const size_t value_size, const size_t buckets, H
     *ht_out = (HT_LP) {
             .buckets = buckets,
             .hash_function = hash_function,
-            .table = table
+            .table = table,
+            .records = records
     };
     return 0;
 }
 
-void HT_LP_Free(HT_LP *hash_table) {
-    for (size_t i = 0U; i != hash_table->buckets; ++i) {
-        free(hash_table->table[i]);
+void HT_LP_Free(HT_LP *ht) {
+    for (size_t i = 0U; i != ht->buckets; ++i) {
+        free(ht->table[i]);
     }
-    free(hash_table->table);
+    free(ht->table);
 }
 
-int HT_LP_Insert(HT_LP *hash_table, Record *record) {
+int HT_LP_Insert(HT_LP *ht, Record *record) {
 #define current_table_element table[outer_probe][inner_probe]
 #define probe_stop_condition (current_table_element.is_empty || current_table_element.is_deleted)
 
-    size_t buckets = hash_table->buckets;
+    size_t buckets = ht->buckets;
     uint16_t outer_bucket_key = (uint16_t) (record->id & 0xFFFF);
-    size_t outer_bucket = hash_table->hash_function(&outer_bucket_key, sizeof(uint16_t), buckets);
+    size_t outer_bucket = ht->hash_function(&outer_bucket_key, sizeof(uint16_t), buckets);
     uint16_t inner_bucket_key = (uint16_t) ((record->id >> sizeof(uint16_t)) & 0xFFFF);
-    size_t inner_bucket = hash_table->hash_function(&inner_bucket_key, sizeof(uint16_t), buckets);
+    size_t inner_bucket = ht->hash_function(&inner_bucket_key, sizeof(uint16_t), buckets);
     size_t outer_probe = outer_bucket;
-    HT_LP_Record_Entry **table = hash_table->table;
+    HT_LP_Record_Entry **table = ht->table;
     uint8_t wrapped = 0U;
     // TODO(gliontos): Find a way to compute offset
     while (!wrapped) {
@@ -61,6 +64,7 @@ int HT_LP_Insert(HT_LP *hash_table, Record *record) {
         if (entry->is_empty || entry->is_deleted) {
             entry->key_value_pair.v1 = inner_bucket_key;
             entry->key_value_pair.v2 = 0U;
+            ht->records[0] = record;
             goto __EXIT;
         }
         size_t inner_probe = (inner_bucket + 1U) % buckets;
@@ -68,6 +72,7 @@ int HT_LP_Insert(HT_LP *hash_table, Record *record) {
             if (probe_stop_condition) {
                 current_table_element.key_value_pair.v1 = inner_bucket_key;
                 current_table_element.key_value_pair.v2 = 0U;
+                ht->records[0] = record;
                 goto __EXIT;
             }
             ++inner_probe;
@@ -81,11 +86,10 @@ int HT_LP_Insert(HT_LP *hash_table, Record *record) {
 }
 
 // TODO(gliontos): Implement this!
-//int HT_LP_Try_Get_Value(HT_LP *ht, const uint64_t id, void *value_out) {
-//    void *array = ht->array;
-//    uint8_t low_key_byte = (uint8_t) (id & 0xFF);
-//    uint8_t high_key_byte = (uint8_t) ((id >> 8U) & 0xFF);
-//    size_t outer_bucket = ht->hash_function(&low_key_byte, sizeof(uint8_t), ht->buckets);
-//    size_t inner_bucket = ht->hash_function(&high_key_byte, sizeof(uint8_t), ht->buckets);
-//    return -1;
-//}
+int HT_LP_Try_Get_Value(HT_LP *ht, const uint64_t id, void *value_out) {
+    uint8_t low_key_byte = (uint8_t) (id & 0xFF);
+    uint8_t high_key_byte = (uint8_t) ((id >> 8U) & 0xFF);
+    size_t outer_bucket = ht->hash_function(&low_key_byte, sizeof(uint8_t), ht->buckets);
+    size_t inner_bucket = ht->hash_function(&high_key_byte, sizeof(uint8_t), ht->buckets);
+    return -1;
+}
--- hash_table_lp.h
+++ hash_table_lp.h
@@ -20,6 +20,8 @@ typedef struct {
 
 typedef struct {
     HT_LP_Record_Entry **table;
+    void **records;
+    size_t record_size;
     size_t buckets;
     Hash_Function hash_function;
 } HT_LP;
@@ -27,19 +29,18 @@ typedef struct {
 /**
  * HT_LP_Create - Creates a new hash table with the given specifications
  * @param ht_out The address where the new hash table will be stored (output parameter)
- * @param value_size The size of the values that will be stored (in bytes)
  * @param buckets The number of buckets the hash table will have
  * @param hash_function The hash function that the hash table will use to hash it's values
  * @return On Success returns 0
  * One of the errors on failure (see error_enums.h for details)
  */
-int HT_LP_Create(HT_LP *ht_out, size_t value_size, size_t buckets, Hash_Function hash_function);
+int HT_LP_Create(HT_LP *ht_out, size_t buckets, Hash_Function hash_function);
 
 /**
  * HT_LP_Free - Deallocates properly the hash table passed as parameter
- * @param hash_table The hash table to deallocate
+ * @param ht The hash table to deallocate
  */
-void HT_LP_Free(HT_LP *hash_table);
+void HT_LP_Free(HT_LP *ht);
 
 /**
  * HT_LP_Try_Get_Value - Tries to retrieve the value with the specified ID
@@ -51,6 +52,6 @@ void HT_LP_Free(HT_LP *hash_table);
  */
 int HT_LP_Try_Get_Value(HT_LP *ht, uint64_t id, void *value_out);
 
-int HT_LP_Insert(HT_LP *hash_table, Record *record);
+int HT_LP_Insert(HT_LP *ht, Record *record);
 
 #endif //DBMS_HASH_TABLE_LP_H
